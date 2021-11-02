b05fa640bfc8c2e236a0d6483e80e862834c4523
Nguyen Duy Tam <duytam.dev@gmail.com>
2021-11-03 06:48:15 +0700

    đồng bộ bottom nav với viewPager, tạo các fragment chính

---
+++ app/src/main/java/com/fpoly/pro1121/userapp/activities/MainActivity.java
@@ -0,0 +1,60 @@
+package com.fpoly.pro1121.userapp.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.viewpager2.widget.ViewPager2;
+
+import android.annotation.SuppressLint;
+import android.os.Bundle;
+
+import com.fpoly.pro1121.userapp.R;
+import com.fpoly.pro1121.userapp.adapter.PagerMainAdapter;
+import com.ismaeldivita.chipnavigation.ChipNavigationBar;
+
+public class MainActivity extends AppCompatActivity {
+
+    ChipNavigationBar bottomNavigationBar;
+    ViewPager2 viewPagerMain;
+    PagerMainAdapter pageMainAdapter;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        initUI();
+        initViewPager();
+    }
+    private void initUI() {
+        bottomNavigationBar = findViewById(R.id.bottom_nav_chip);
+        viewPagerMain = findViewById(R.id.viewpager2_main);
+    }
+
+    private void initViewPager() {
+        pageMainAdapter = new PagerMainAdapter(this);
+        viewPagerMain.setAdapter(pageMainAdapter);
+        viewPagerMain.registerOnPageChangeCallback(new ViewPager2.OnPageChangeCallback() {
+            @Override
+            public void onPageSelected(int position) {
+                super.onPageSelected(position);
+                // connect với bottom nav
+                switch(position) {
+                    case 0:bottomNavigationBar.setItemSelected(R.id.action_home,true); break;
+                    case 1:bottomNavigationBar.setItemSelected(R.id.action_cart,true); break;
+                    case 2:bottomNavigationBar.setItemSelected(R.id.action_account,true); break;
+                    default: bottomNavigationBar.setItemSelected(R.id.action_home,true); break;
+                }
+            }
+        });
+        bottomNavigationBar.setOnItemSelectedListener(new ChipNavigationBar.OnItemSelectedListener() {
+            @SuppressLint("NonConstantResourceId")
+            @Override
+            public void onItemSelected(int i) {
+                // connect với viewpager2
+                switch (i){
+                    case R.id.action_home:viewPagerMain.setCurrentItem(0); break;
+                    case R.id.action_cart:viewPagerMain.setCurrentItem(1); break;
+                    case R.id.action_account:viewPagerMain.setCurrentItem(2); break;
+                    default: viewPagerMain.setCurrentItem(0); break;
+                }
+            }
+        });
+    }
+}
---
+++ app/src/main/java/com/fpoly/pro1121/userapp/fragment/CartFragment.java
@@ -0,0 +1,24 @@
+package com.fpoly.pro1121.userapp.fragment;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import com.fpoly.pro1121.userapp.R;
+
+public class CartFragment extends Fragment {
+
+    View mView;
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        mView = inflater.inflate(R.layout.fragment_cart,container,false);
+        return mView;
+
+    }
+}
