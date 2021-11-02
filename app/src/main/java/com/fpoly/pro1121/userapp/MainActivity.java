d948f9b068dd1a478993d09b4378bfff6d9f016e
Nguyen Duy Tam <duytam.dev@gmail.com>
2021-11-02 18:16:23 +0700

    Initial commit

---
+++ app/src/main/java/com/fpoly/pro1121/userapp/MainActivity.java
@@ -0,0 +1,14 @@
+package com.fpoly.pro1121.userapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
