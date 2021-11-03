a46c1e01e7a327cb412486bc1954ca49d7351e6f
Nguyen Duy Tam <duytam.dev@gmail.com>
2021-11-03 22:47:10 +0700

    login, đăng kí bằng email - pass firebase

--- app/src/main/java/com/fpoly/pro1121/userapp/activities/LoginActivity.java
+++ app/src/main/java/com/fpoly/pro1121/userapp/activities/LoginActivity.java
@@ -1,5 +1,6 @@
 package com.fpoly.pro1121.userapp.activities;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
@@ -7,19 +8,26 @@ import android.os.Bundle;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.fpoly.pro1121.userapp.R;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.android.material.textfield.TextInputLayout;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class LoginActivity extends AppCompatActivity {
 
     EditText edtEmail,edtPassword;
     Button btnLogin;
     TextView tvSignup;
+    private FirebaseAuth mAuth;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        mAuth = FirebaseAuth.getInstance();
         initUI();
         events();
     }
@@ -34,10 +42,32 @@ public class LoginActivity extends AppCompatActivity {
     }
     private void events() {
         tvSignup.setOnClickListener(view -> {
-            startActivity(new Intent(this, LoginActivity.class));
+            startActivity(new Intent(this, RegisterActivity.class));
         });
         btnLogin.setOnClickListener(view -> {
-            startActivity(new Intent(this, MainActivity.class));
+            String email = edtEmail.getText().toString();
+            String password = edtPassword.getText().toString();
+            actionSignIn(email,password);
         });
+    }
+
+    private void actionSignIn(String email,String password) {
+        mAuth.signInWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            // Sign in success, update UI with the signed-in user's information
+                            Toast.makeText(LoginActivity.this, "Đăng nhập thành công .",
+                                    Toast.LENGTH_SHORT).show();
+                            startActivity(new Intent(LoginActivity.this, MainActivity.class));
+                        } else {
+                            // If sign in fails, display a message to the user.
+
+                            Toast.makeText(LoginActivity.this, "Authentication failed.",
+                                    Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
     }
 }
