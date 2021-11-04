2a12328087c052ab0578fae4a507ed4748088c77
Nguyen Duy Tam <duytam.dev@gmail.com>
2021-11-04 13:10:12 +0700

    get category from server to view app

--- app/src/main/java/com/fpoly/pro1121/userapp/fragment/HomeFragment.java
+++ app/src/main/java/com/fpoly/pro1121/userapp/fragment/HomeFragment.java
@@ -11,13 +11,27 @@ import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.widget.SearchView;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import com.fpoly.pro1121.userapp.R;
 import com.fpoly.pro1121.userapp.activities.SearchProductActivity;
+import com.fpoly.pro1121.userapp.adapter.CategoryAdapter;
 import com.fpoly.pro1121.userapp.adapter.SliderAdapter;
+import com.fpoly.pro1121.userapp.model.Category;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.EventListener;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.FirebaseFirestoreException;
+import com.google.firebase.firestore.QuerySnapshot;
 import com.smarteist.autoimageslider.IndicatorView.animation.type.IndicatorAnimationType;
 import com.smarteist.autoimageslider.SliderAnimations;
 import com.smarteist.autoimageslider.SliderView;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.Objects;
 
 public class HomeFragment extends Fragment {
 
@@ -26,6 +40,10 @@ public class HomeFragment extends Fragment {
     TextView tvHelloUser;
     SliderAdapter sliderAdapter;
     SliderView sliderView;
+    RecyclerView rvCategory;
+    CategoryAdapter categoryAdapter;
+    List<Category> listCategories;
+    private FirebaseFirestore db = FirebaseFirestore.getInstance();
     int[] imagesSlide ={R.drawable.banner,R.drawable.banner,R.drawable.banner,R.drawable.banner};
     @Nullable
     @Override
@@ -33,17 +51,48 @@ public class HomeFragment extends Fragment {
         mView = inflater.inflate(R.layout.fragment_home,container,false);
         initUI();
         initSlider();
+        initRecyclerCategory();
         actionSearch();
+        realTimeDataBase();
         return mView;
 
     }
 
+    private void realTimeDataBase() {
+        db.collection("categories")
+                .addSnapshotListener(new EventListener<QuerySnapshot>() {
+                    @Override
+                    public void onEvent(@Nullable QuerySnapshot value, @Nullable FirebaseFirestoreException error) {
+                        if(value!=null){
+                            try {
+                                List<Category> clones = new ArrayList<>();
+                                List<DocumentSnapshot> snapshotsList = value.getDocuments();
+                                for(DocumentSnapshot snapshot:  snapshotsList){
+                                    Map<String, Object> data = snapshot.getData();
+                                    assert data != null;
+                                    String id = Objects.requireNonNull(data.get("id")).toString();
+                                    String name = Objects.requireNonNull(data.get("name")).toString();
+                                    String urlImage = Objects.requireNonNull(data.get("urlImage")).toString();
+                                    Category category = new Category(id,name,urlImage);
+                                    clones.add(category);
+                                }
+                                listCategories = new ArrayList<>();
+                                listCategories.addAll(clones);
+                                categoryAdapter.setData(listCategories);
+                            }catch(Exception e) {
+                                e.printStackTrace();
+                            }
+                        }
+                    }
+                });
+    }
 
 
     private void initUI() {
         mSearch = mView.findViewById(R.id.search_main_fragment);
         mSearch.setQueryHint("Search");
         tvHelloUser = mView.findViewById(R.id.tv_hello_user);
+        rvCategory = mView.findViewById(R.id.rv_category);
     }
 
     private void initSlider() {
@@ -53,6 +102,15 @@ public class HomeFragment extends Fragment {
         sliderView.setIndicatorAnimation(IndicatorAnimationType.WORM);
         sliderView.setSliderTransformAnimation(SliderAnimations.DEPTHTRANSFORMATION);
         sliderView.startAutoCycle();
+    }
+
+    private void initRecyclerCategory() {
+        categoryAdapter = new CategoryAdapter();
+        categoryAdapter.setData(listCategories);
+        rvCategory.setAdapter(categoryAdapter);
+        LinearLayoutManager linearLayoutManager = new LinearLayoutManager(requireContext(),RecyclerView.HORIZONTAL,false);
+        rvCategory.setLayoutManager(linearLayoutManager);
+
     }
     private void actionSearch() {
         mSearch.setOnClickListener(new View.OnClickListener() {
