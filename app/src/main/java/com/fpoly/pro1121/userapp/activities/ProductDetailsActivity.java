32c26e0c5a10590518e611830cbf4251252405f1
Nguyen Duy Tam <duytam.dev@gmail.com>
2021-11-04 15:46:14 +0700

    update UI login, product detail

--- app/src/main/java/com/fpoly/pro1121/userapp/activities/ProductDetailsActivity.java
+++ app/src/main/java/com/fpoly/pro1121/userapp/activities/ProductDetailsActivity.java
@@ -4,18 +4,16 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.widget.Button;
-import android.widget.EditText;
-import android.widget.TextView;
 
 import com.fpoly.pro1121.userapp.R;
 
-public class productActivity extends AppCompatActivity {
+public class ProductDetailsActivity extends AppCompatActivity {
 
     Button btnAddCart;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_product);
+        setContentView(R.layout.activity_product_details);
 
         btnAddCart = (Button) findViewById(R.id.btnAddCart);
     }
