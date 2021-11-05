030a2bada7d8a25ba68c8bcaf0d013ab772dafda
luanlbps16836 <luanlbps16836@fpt.edu.vn>
2021-11-05 15:29:50 +0700

    cmt add activity_edit_profile ui

---
+++ app/src/main/java/com/fpoly/pro1121/userapp/activities/EditProfileActivity.java
@@ -0,0 +1,16 @@
+package com.fpoly.pro1121.userapp.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.fpoly.pro1121.userapp.R;
+
+public class EditProfileActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_edit_profile);
+    }
+}
